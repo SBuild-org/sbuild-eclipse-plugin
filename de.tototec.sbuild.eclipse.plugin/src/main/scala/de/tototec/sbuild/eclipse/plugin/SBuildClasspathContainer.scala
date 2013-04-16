056ac5a2d0608d9a6593eae9d7c66967ccf9d9b6
Tobias Roeser <le.petit.fou@web.de>
2013-04-16 12:39:53 +0000

    Improved classpath container update after detected changes.

--- de.tototec.sbuild.eclipse.plugin/src/main/scala/de/tototec/sbuild/eclipse/plugin/SBuildClasspathContainer.scala
+++ de.tototec.sbuild.eclipse.plugin/src/main/scala/de/tototec/sbuild/eclipse/plugin/SBuildClasspathContainer.scala
@@ -64,17 +64,19 @@ class SBuildClasspathContainer(path: IPath, val project: IJavaProject) extends I
   protected var relatedWorkspaceProjectNames: Set[String] = Set()
   def dependsOnWorkspaceProjects(projectNames: Array[String]) = relatedWorkspaceProjectNames.exists(name => projectNames.contains(name))
 
-  def this(predecessor: SBuildClasspathContainer) {
-    this(predecessor.getPath, predecessor.project)
-    this.classpathEntries = predecessor.classpathEntries
-    this.relatedWorkspaceProjectNames = predecessor.relatedWorkspaceProjectNames
-  }
+//  def this(predecessor: SBuildClasspathContainer) {
+//    this(predecessor.getPath, predecessor.project)
+//    this.classpathEntries = predecessor.classpathEntries
+//    this.relatedWorkspaceProjectNames = predecessor.relatedWorkspaceProjectNames
+//  }
 
   def notifyUpdateClasspathEntries(inBackground: Boolean = false) {
     def notify = {
       try {
-        val newContainer = new SBuildClasspathContainer(this)
+        val newContainer = this // new SBuildClasspathContainer(this)
         JavaCore.setClasspathContainer(path, Array(project), Array(newContainer), new NullProgressMonitor())
+        // next line is the long searched-for magic?
+        project.setRawClasspath(project.getRawClasspath(), new NullProgressMonitor())
       } catch {
         case e: Throwable => error("Caught exception while updating the SBuildClasspathContainer for project: " + project, e)
       }
