c6ffaed41bf0e7cf6cfb40cd520361b343b95da4
Tobias Roeser <le.petit.fou@web.de>
2014-08-09 15:17:09 +0200

    Added listener to preferences changes which re-register the service with changed settings.

--- org.sbuild.eclipse.resolver.sbuild07/src/main/scala/org/sbuild/eclipse/resolver/sbuild07/internal/SBuild07ResolverActivator.scala
+++ org.sbuild.eclipse.resolver.sbuild07/src/main/scala/org/sbuild/eclipse/resolver/sbuild07/internal/SBuild07ResolverActivator.scala
@@ -7,6 +7,9 @@ import org.sbuild.eclipse.resolver.SBuildResolver
 import java.util.Properties
 import java.io.File
 import org.eclipse.ui.plugin.AbstractUIPlugin
+import org.eclipse.jface.util.IPropertyChangeListener
+import org.osgi.framework.ServiceRegistration
+import org.eclipse.jface.util.PropertyChangeEvent
 
 object SBuild07ResolverActivator {
   private var _activator: Option[SBuild07ResolverActivator] = None
@@ -15,24 +18,46 @@ object SBuild07ResolverActivator {
 
 class SBuild07ResolverActivator extends AbstractUIPlugin with BundleActivator {
 
+  private[this] var onStop: List[BundleContext => Unit] = Nil
+
   override def start(context: BundleContext): Unit = {
     SBuild07ResolverActivator._activator = Some(this)
     super.start(context)
 
-    val sbuildHome = getPreferenceStore().getString("SBUILD_PATH")
-    if (sbuildHome != null && sbuildHome.trim().length() > 0) {
-      val resolver = new SBuild07Resolver(new File(sbuildHome))
-      // TODO: Register a pref change listener, and reregister the service with the changed path
+    var sbuildHome: String = ""
+    var ref: Option[ServiceRegistration] = None
 
-      // TODO: Use the content of the SBUILD_HOME classpath variable as default for the preferences, if set
-
-      // TODO: add extra info about resolver in properties
-      context.registerService(classOf[SBuildResolver].getName, resolver, null)
+    def updateSBuildHome(newSBuildHome: String): Unit = {
+      if (newSBuildHome != sbuildHome) {
+        sbuildHome = newSBuildHome
+        ref foreach { _.unregister() }
+        ref = if (sbuildHome != null && sbuildHome.trim().length() > 0) {
+          val resolver = new SBuild07Resolver(new File(sbuildHome))
+          // TODO: Use the content of the SBUILD_HOME classpath variable as default for the preferences, if set
+          // TODO: add extra info about resolver in properties
+          Some(context.registerService(classOf[SBuildResolver].getName, resolver, null))
+        } else None
+      }
     }
+
+    val propChangeListener = new IPropertyChangeListener {
+      override def propertyChange(event: PropertyChangeEvent): Unit = {
+        event.getProperty match {
+          case "SBUILD_PATH" => updateSBuildHome(event.getNewValue().asInstanceOf[String])
+          case _ =>
+        }
+      }
+    }
+    getPreferenceStore().addPropertyChangeListener(propChangeListener)
+
+    onStop ::= { _ => getPreferenceStore().removePropertyChangeListener(propChangeListener) }
+
+    updateSBuildHome(getPreferenceStore().getString("SBUILD_PATH"))
 
   }
 
   override def stop(context: BundleContext): Unit = {
+    onStop foreach { f => f(context) }
     SBuild07ResolverActivator._activator = None
     super.stop(context)
   }
--- org.sbuild.eclipse.resolver.sbuild08/src/main/scala/org/sbuild/eclipse/resolver/sbuild08/internal/SBuild08ResolverActivator.scala
+++ org.sbuild.eclipse.resolver.sbuild08/src/main/scala/org/sbuild/eclipse/resolver/sbuild08/internal/SBuild08ResolverActivator.scala
@@ -7,6 +7,9 @@ import org.sbuild.eclipse.resolver.SBuildResolver
 import java.util.Properties
 import java.io.File
 import org.eclipse.ui.plugin.AbstractUIPlugin
+import org.osgi.framework.ServiceRegistration
+import org.eclipse.jface.util.IPropertyChangeListener
+import org.eclipse.jface.util.PropertyChangeEvent
 
 object SBuild08ResolverActivator {
   private var _activator: Option[SBuild08ResolverActivator] = None
@@ -15,21 +18,45 @@ object SBuild08ResolverActivator {
 
 class SBuild08ResolverActivator extends AbstractUIPlugin with BundleActivator {
 
+ private[this] var onStop: List[BundleContext => Unit] = Nil
+
   override def start(context: BundleContext): Unit = {
     SBuild08ResolverActivator._activator = Some(this)
     super.start(context)
 
-    val sbuildHome = getPreferenceStore().getString("SBUILD_PATH")
-    if (sbuildHome != null && sbuildHome.trim().length() > 0) {
-      val resolver = new SBuild08Resolver(new File(sbuildHome))
-      // TODO: Register a pref change listener, and reregister the service with the changed path
-      // TODO: add extra info about resolver in properties
-      context.registerService(classOf[SBuildResolver].getName, resolver, null)
+    var sbuildHome: String = ""
+    var ref: Option[ServiceRegistration] = None
+
+    def updateSBuildHome(newSBuildHome: String): Unit = {
+      if (newSBuildHome != sbuildHome) {
+        sbuildHome = newSBuildHome
+        ref foreach { _.unregister() }
+        ref = if (sbuildHome != null && sbuildHome.trim().length() > 0) {
+          val resolver = new SBuild08Resolver(new File(sbuildHome))
+          // TODO: add extra info about resolver in properties
+          Some(context.registerService(classOf[SBuildResolver].getName, resolver, null))
+        } else None
+      }
     }
+
+    val propChangeListener = new IPropertyChangeListener {
+      override def propertyChange(event: PropertyChangeEvent): Unit = {
+        event.getProperty match {
+          case "SBUILD_PATH" => updateSBuildHome(event.getNewValue().asInstanceOf[String])
+          case _ =>
+        }
+      }
+    }
+    getPreferenceStore().addPropertyChangeListener(propChangeListener)
+
+    onStop ::= { _ => getPreferenceStore().removePropertyChangeListener(propChangeListener) }
+
+    updateSBuildHome(getPreferenceStore().getString("SBUILD_PATH"))
 
   }
 
   override def stop(context: BundleContext): Unit = {
+    onStop foreach { f => f(context) }
     SBuild08ResolverActivator._activator = None
     super.stop(context)
   }
